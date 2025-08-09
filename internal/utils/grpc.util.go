251dd548fb74511c454cde990a7093b89e874e3c
sinh <sinh@gmail.com>
2025-08-09 16:33:16 +0700

    [PAGING] SCD-43: Add page ignore and sort logic in pagingation

--- internal/utils/grpc.util.go
+++ internal/utils/grpc.util.go
@@ -10,6 +10,16 @@ import (
 func ToPageQuery(c *gin.Context) *common.PageQuery {
 	page := c.Query("page")
 	pageSize := c.Query("page_size")
+	var sortBy *string
+
+	pageIgnore := c.Query("page_ignore") == "true"
+
+	sortByValue := c.Query("sort_by")
+	if sortByValue != "" {
+		sortBy = &sortByValue
+	} else {
+		sortBy = nil
+	}
 
 	if page == "" {
 		page = "1"
@@ -19,18 +29,20 @@ func ToPageQuery(c *gin.Context) *common.PageQuery {
 		pageSize = "10"
 	}
 
-	// Convert page and pageSize to integers
 	pageInt, err := strconv.Atoi(page)
 	if err != nil {
-		pageInt = 1 // Default to 1 if conversion fails
+		pageInt = 1
 	}
+
 	pageSizeInt, err := strconv.Atoi(pageSize)
 	if err != nil {
-		pageSizeInt = 10 // Default to 10 if conversion fails
+		pageSizeInt = 10
 	}
 
 	return &common.PageQuery{
-		Page:     int32(pageInt),
-		PageSize: int32(pageSizeInt),
+		Page:       int32(pageInt),
+		PageSize:   int32(pageSizeInt),
+		SortBy:     sortBy,
+		PageIgnore: &pageIgnore,
 	}
 }
