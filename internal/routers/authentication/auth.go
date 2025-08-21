84832df5265118ab59fda1f5d11f8c0289b92893
sinh <sinh@gmail.com>
2025-08-21 20:21:48 +0700

    [CSRF] SCD-58: Fix lack of saved token to send cookie to client

--- internal/routers/authentication/auth.go
+++ internal/routers/authentication/auth.go
@@ -3,6 +3,7 @@ package authentication
 import (
 	controller "schedule_gateway/internal/controller/auth"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	csrf "github.com/utrack/gin-csrf"
 )
@@ -17,6 +18,12 @@ func (ar *AuthRouter) InitAuthRouter(routerGroup *gin.RouterGroup) {
 	{
 		authRouterPublic.POST("login-with-google", authController.LoginWithGoogle)
 		authRouterPublic.GET("csrf-token", func(ctx *gin.Context) {
+			session := sessions.Default(ctx)
+			session.Set("init", true)
+			if err := session.Save(); err != nil {
+				ctx.JSON(500, gin.H{"error": "Failed to save session"})
+				return
+			}
 			ctx.JSON(200, gin.H{
 				"csrf_token": csrf.GetToken(ctx),
 			})
